57e675cad01fcd1e85dd1c4d8222a07573b2c371
Huy Nguyen <huynv@lotte.vn>
2019-05-29 16:13:01 +0700

    ok

---
+++ pages/_lang/Policy.vue
@@ -0,0 +1,201 @@
+<template>
+  <div class="policy">
+    <header-banner title="Privacy Policy" :img="headerImage"></header-banner>
+    <div class="container">
+      <p>Lotte <span lang="VI">Coralis Vietnam</span><span lang="VI"> </span><span lang="VI">Co., LTD </span>prime importance on customer personal information, and
+complies with "the Personal Information Protection Act." Lotte
+Coralis informs guests of means and purpose for which personal information
+provided have been used, and what measures we have taken to protect personal
+information according to our privacy policy.</p>
+
+<p><strong>Article 1. Scope and methods of collecting personal information</strong><o:p></o:p></p>
+
+<p>1.<span lang="VI">1</span> Scope of collecting personal information<o:p></o:p></p>
+
+<p>Lotte Coralis may collect minimal personal
+information in order to provide proper service.<span lang="VI"><o:p></o:p></span></p>
+
+<p>1.<span lang="VI">2</span> Methods of collecting personal information<o:p></o:p></p>
+
+<p>Lotte <span lang="VI">Coralis</span> may collect personal
+information in the following ways.<o:p></o:p></p>
+
+<p>Website registration, in written form
+(application for membership<span lang="VI">,
+booking, rental contract, guest registration</span>)</p>
+
+<p><strong>Article
+2. Personal information collection, use purposes<span lang="VI"> and retention
+period</span></strong><span lang="VI"><o:p></o:p></span></p>
+
+<p>Lotte
+Coralis collects personal information to provide services, as follows:<o:p></o:p></p>
+
+<p><span lang="VI">1. Sign-up online for leasing <o:p></o:p></span></p>
+
+<p><span lang="VI">- Obtaining items: Name, Phone number,
+E-mail<o:p></o:p></span></p>
+
+<p><span lang="VI">- Obtaining purpose: To provide service
+registration<o:p></o:p></span></p>
+
+<p><span lang="VI">- Retention period: 1 years<o:p></o:p></span></p>
+
+<p><span lang="VI">- Rights of refusal and disadvantages:
+Customer can refuse the consent, but cannot sign up for homepage<o:p></o:p></span></p>
+
+<p><span lang="VI">2. Sign-up by offline document: service
+resident<o:p></o:p></span></p>
+
+<p><span lang="VI">- Obtaining items : Name, Phone number,
+E-mail, Address, Gender, Age, ID/passport, nationality, credit card<o:p></o:p></span></p>
+
+<p><span lang="VI">- Obtaining purpose: To provide room
+reservation, personal identification, payment guarantee and Customer
+Counselling<o:p></o:p></span></p>
+
+<p><span lang="VI">- Retention period: 2 year after stay
+and 5 days after canceling date if canceling reservation<o:p></o:p></span></p>
+
+<p><span lang="VI">- Rights of refusal and disadvantages:
+Customer can refuse the consent, but limitation of services.<o:p></o:p></span></p>
+
+<p><span lang="VI">3. Sign-up by offline document:&nbsp; fitness membership<o:p></o:p></span></p>
+
+<p><span lang="VI">- Obtaining items: Name, Phone number,
+E-mail, Address, Gender, Age, ID/passport, nationality<o:p></o:p></span></p>
+
+<p><span lang="VI">- Obtaining purpose: To provide
+membership service and manage membership<o:p></o:p></span></p>
+
+<p><span lang="VI">- Retention period: 2 year after
+membership withdrawal<o:p></o:p></span></p>
+
+<p><span lang="VI">- Rights of refusal and disadvantages:
+Customer can refuse the consent, but limitation of services.<o:p></o:p></span></p>
+
+<p><span lang="VI">4. Sign-up by offline document: Office
+leasing<o:p></o:p></span></p>
+
+<p><span lang="VI">- Obtaining items: business license, tax
+certificate, email, contact number<o:p></o:p></span></p>
+
+<p><span lang="VI">- Obtaining purpose: Cooperate
+information are collected to provide information to relevant Lotte internal
+Department and to create Lease Agreement.<o:p></o:p></span></p>
+
+<p><span lang="VI">- Retention period: 10 year <o:p></o:p></span></p>
+
+<p><span lang="VI">- Rights of refusal and disadvantages:
+Customer can refuse the consent, but impact of Lease Agreement. <o:p></o:p></span></p>
+
+<p><span lang="VI">5. Sign-up when enter/exit the building<o:p></o:p></span></p>
+
+<p><span lang="VI">- Obtaining items: Name, Phone number,
+E-mail, ID number<o:p></o:p></span></p>
+
+<p><span lang="VI">- Obtaining purpose: the building safety
+and security<o:p></o:p></span></p>
+
+<p><span lang="VI">- Retention period: 2 years<o:p></o:p></span></p>
+
+<p><span lang="VI">- Rights of refusal and disadvantages:
+Customer can refuse the consent, but cannot enter</span></p>
+
+<p><strong>Article
+3. Personal information disposal methods</strong><o:p></o:p></p>
+
+<p>Personal
+information printed on paper: to be cut into pieces by a shredder or
+incinerated<o:p></o:p></p>
+
+<p>Personal
+information stored in electronic files: to be deleted using a technical method
+that makes it impossible to regenerate it</p>
+
+<p><strong>Article
+4. Entrusting and handling of personal information</strong><o:p></o:p></p>
+
+<p>Upon
+making an entrustment contract, Lotte Coralis sets forth clearly strict
+compliance with instructions related to privacy, prohibitions on personal
+information, responsibility for accidents, etc. and keeps the contents of such
+contracts and form in writing, for the safe protection of personal information.
+<o:p></o:p></p>
+
+<p>&nbsp;The Company will only provide personal
+information for 3rd party when disputes occur as legal consultant may need to
+involved and supervise third party service providers observing specified
+matters properly within the scope of activities to be entrusted to ensure that
+personal information subject to such entrustment is managed safely.</p>
+
+<p><strong>Article
+5. Methods of reading and correcting personal information, canceling
+membership, and withdrawing consent</strong><o:p></o:p></p>
+
+<p>Customers
+may read or correct their registered personal information or cancel the
+membership at any time. If customers make a request for correcting an error in
+their personal information, such information will not be used or provided until
+such correction is finished. Also, if erroneous personal information was
+already provided to a third party, we will correct the error by immediately
+notifying the third party of modified information.</p>
+
+<p><strong>Article 6. Personal information
+administrator</strong><o:p></o:p></p>
+
+<p>Lotte
+Coralis appoints personal information administrators as follows to protect
+members' personal information, to collect their opinions, and handle
+complaints, about such information:<o:p></o:p></p>
+
+<p>Department
+in charge of personal information: Marketing Team, Planning Team and IT Team<o:p></o:p></p>
+
+<p>-
+Personal Information Officer: Mai Hoang (Tel: 024-3333-6013&nbsp; Email: maihoang@lotte.vn)<o:p></o:p></p>
+
+<p>- &nbsp;Marketing division Officer: Thuan Nguyen(Tel:
+024-3333-6023 Email: <a href="mailto:thuannguyen@lotte.vn">thuannguyen@lotte.vn</a>)<o:p></o:p></p>
+
+<p>- IT
+Division Officer: Dinh Do (Tel : 024-3333-6059 Email: baodinh@lotte.vn)</p>
+
+<p>This
+Privacy Policy was established on May 1st ,2019, and any addition to, deletion
+of, and modification to the Policy made in accordance with changes to laws,
+regulations, policies or security technologies will be announced on the website
+of our main office.</p>
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+import HeaderBanner from '~/components/HeaderBanner.vue'
+export default {
+  components: {
+    'header-banner': HeaderBanner
+  },
+  data () {
+    return {
+      headerImage: '/img/bg/overview.png'
+    }
+  },
+  head () {
+    return {
+      title: 'Privacy Policy',
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        { hid: 'description', name: 'description', content: 'Privacy Policy' }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
+
+
---
+++ pages/policy.vue
@@ -0,0 +1,4 @@
+<script>
+import Policy from '~/pages/_lang/Policy'
+export default Policy
+</script>
