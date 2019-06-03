145c526174f513e19c7577802bf3f6de551a910e
Huy Nguyen <huynv@lotte.vn>
2019-06-03 15:44:28 +0700

    ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ module.exports = {
   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
-    // baseURL: '/'
+    baseURL: 'http://observationdeck.lottecenter.com.vn/'
   },
 
   /*
